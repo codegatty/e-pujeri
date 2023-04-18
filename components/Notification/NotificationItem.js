f30ee5ab8c0719c959f38520694226c1c85e6ef2
lenovo <rajithgatty61@gmail.com>
2023-04-18 07:28:09 +0530

    badge updated
    badge updated

--- components/Notification/NotificationItem.js
+++ components/Notification/NotificationItem.js
@@ -3,6 +3,8 @@ import { Text,Pressable,View,StyleSheet} from "react-native";
 
 import { dateToString } from "../../util/others/dateToString";
 import { globalColors } from "../../constants/appColors";
+import Tag from "../ui/Tag";
+import { findDiffBetweenDates } from "../../util/others/findDiffBetweenDates";
 function NotificationItem({data}){
     const Navigation=useNavigation();
     function onPressHandler(){
@@ -19,11 +21,35 @@ function NotificationItem({data}){
         }
         Navigation.navigate("NotificationSummery",{notiData:notificationData});
     }
+
+    
+    function tagHandler(){
+        if(data.notficationType==='event'){
+            const diff=findDiffBetweenDates(data.date,new Date())
+            if(diff<=1)
+                return <Tag>Important</Tag>
+            else
+                return ''
+        }else{
+            const diff=findDiffBetweenDates(data.publishedDate,new Date())
+            if(diff==0)
+                return <Tag>Important</Tag>
+            else
+                return ''
+        }
+    }
 return(
     <Pressable style={styles.pressable} onPress={onPressHandler}>
     <View style={styles.detailContainer}>
         <Text style={styles.detail}> {data.name}</Text>
         <Text style={styles.detail}>PublishedDate: {dateToString(data.publishedDate)}</Text>
+    </View>
+    <View style={styles.mainTagContainer}>
+        <View style={styles.tagContainer}>
+        {tagHandler()}
+        <Tag style1={[data.notficationType==='event'?{backgroundColor:'green'}:{backgroundColor:'blue'},styles.tag]}>
+            {data.notficationType==='event'?'Event':'Announcemnet'}</Tag>
+        </View>
     </View>
 </Pressable>
 );
@@ -42,5 +68,14 @@ const styles = StyleSheet.create({
     },
     detail: {
         flex: 1,
+    },
+    mainTagContainer:{
+        flexDirection:'column',
+    },
+    tagContainer:{
+        flexDirection:'row-reverse',
+        marginLeft:10
+    },tag:{
+        marginLeft:8
     }
 })
--- components/ui/Tag.js
+++ components/ui/Tag.js
@@ -1,5 +1,31 @@
-function Tag(){
+import {View,Text,StyleSheet} from 'react-native';
+import { color } from 'react-native-reanimated';
+
+function Tag({children,style1,style2}){
+    return(
+        <View style={[styles.main,style1]}>
+            <Text style={[styles.text,style2]}>
+                {children}
+            </Text>
+        </View>
+    )
 
 }
 
 export default Tag;
+
+const styles=StyleSheet.create({
+main:{
+    backgroundColor:'red',
+    borderRadius:6,
+    borderWidth:1,
+    
+},
+text:{
+    paddingHorizontal:5,
+    paddingVertical:2,
+    fontSize:10,
+    color:'white'
+}
+
+})
